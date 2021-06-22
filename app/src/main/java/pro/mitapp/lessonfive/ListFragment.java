f2d5468353db1c5607b69be0da4a8b12d76ce04d
Adeline16 <urmanbetova502@gmail.com>
2021-06-22 18:45:58 +0600

    Some mistakes was resolved

--- app/src/main/java/pro/mitapp/lessonfive/ListFragment.java
+++ app/src/main/java/pro/mitapp/lessonfive/ListFragment.java
@@ -58,13 +58,6 @@ public class ListFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        btn_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
 
 
     }
